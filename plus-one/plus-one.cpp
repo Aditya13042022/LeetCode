fd29841c3b01811ce9925b3d91eed924678bada4
Aditya13042022 <106468168+Aditya13042022@users.noreply.github.com>
2022-06-02 15:12:59 +0530

    Time: 0 ms, Memory: 8.9 MB - LeetHub

---
+++ plus-one/plus-one.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        int index=n-1;
+        while(index>=0 && digits[index]==9 )
+            digits[index--]=0;
+        if(index<0)
+            digits.insert(digits.begin(),1,1);
+        else
+            digits[index]++;
+        return digits;
+        
+    }
+};
