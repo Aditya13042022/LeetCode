023a12924b083f5392d038d6cd8ba63dbaed8c20
Thakur Aditya Singh <106468168+Aditya13042022@users.noreply.github.com>
2022-08-08 23:08:05 +0530

    Time: 8 ms (48.46%), Space: 9.7 MB (22.59%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-int ans=0;
-        int size=nums.size();
-        if(target>nums[size-1]){
+        if(target>nums[nums.size()-1]){
             return nums.size();
         }
         int i=0;
