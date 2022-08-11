201ab52d164ec430263a1283c37b40dd9450e7ab
Thakur Aditya Singh <106468168+Aditya13042022@users.noreply.github.com>
2022-08-12 03:18:14 +0530

    Time: 193 ms (90.74%), Space: 77.2 MB (52.31%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int,int>solve;
+         unordered_map<int,int>solve;
         vector<int>ans;
+        int sub;
+        int check=0;
      for(int i=0; i<nums.size();i++)
      {
-         if(solve.find(nums[i])!=solve.end())
+        
+         sub=nums[i];
+         if(solve.find(sub)!=solve.end())
          {
              
              ans.push_back(i);
-             ans.push_back(solve[nums[i]]);
-             int j=solve[nums[i]];
-
-             if(abs(i-j)<=k)
+             ans.push_back(solve[sub]);
+             int j=solve[sub];
+             check=abs(i-j);
+             if(check<=k)
              {
                  return true;
              }
